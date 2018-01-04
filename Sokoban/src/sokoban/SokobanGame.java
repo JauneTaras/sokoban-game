3a2f807db19603f229ef5962768ae20bdf490974
JauneTaras <32001298+JauneTaras@users.noreply.github.com>
2018-01-04 15:01:46 +0000

    Start of the game
    Swing Frame

---
+++ Sokoban/src/sokoban/SokobanGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sokoban;
+import javax.swing.*;
+/**
+ *
+ * @author Jaune
+ */
+public class SokobanGame {
+    
+      
+        
+      SokobanGame(){
+       JFrame mainWindow = new JFrame();
+        
+        mainWindow.setSize(500,500);
+        mainWindow.setVisible(true);
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      }  
+        
+        
+}
